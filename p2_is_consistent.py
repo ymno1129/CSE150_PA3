d178ea03075b7e7c3311400f6e4ba4646fcf1d2e
ymno1129 <huzhuang@ucsd.edu>
2016-05-03 10:49:51 -0700

    p1&p2

---
+++ p2_is_consistent.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+
+def is_consistent(csp, variable, value):
+    """Returns True when the variable assignment to value is consistent, i.e. it does not violate any of the constraints
+    associated with the given variable for the variables that have values assigned.
+
+    For example, if the current variable is X and its neighbors are Y and Z (there are constraints (X,Y) and (X,Z)
+    in csp.constraints), and the current assignment as Y=y, we want to check if the value x we want to assign to X
+    violates the constraint c(x,y).  This method does not check c(x,Z), because Z is not yet assigned."""
+
+    # TODO implement this
+    result = True
+    variable.assign(value)
+    for x in csp.constraints:
+        if not x.is_satisfied(x.var1.value, x.var2.value):
+            result = False
+    return result
+    pass
